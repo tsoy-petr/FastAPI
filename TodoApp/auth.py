e473261e8d70f7438ebf31135757cc405a9c9d91
bildovich <bildovich@yandex.ru>
2021-12-04 20:14:29 +1000

    fastapi init 2

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -5,7 +5,12 @@ import models
 from passlib.context import CryptContext
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "WmGzSW4ExLhYAkdtsShDm1VtLLFb4euW"
+ALGORITHM = "HS256"
 
 
 class CreateUser(BaseModel):
@@ -17,7 +22,10 @@ class CreateUser(BaseModel):
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 models.Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -50,6 +58,16 @@ def authenticate_user(username: str, password: str, db):
     return user
 
 
+def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
+    encode = {"sub": username, "id": user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({"expire": expire.__str__()})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
 @app.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
@@ -69,4 +87,6 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=404, detail="User net found")
-    return "User Validate"
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(user.username, user.id, expires_delta=token_expires)
+    return {"token": token}
